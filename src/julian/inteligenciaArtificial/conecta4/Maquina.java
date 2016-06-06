515651f844db5a98f222669794c0e35a979bf2f1
Julian <jtoledanodiaz@gmail.com>
2016-06-06 23:34:10 +0200

    Maquina sin IA

--- src/julian/inteligenciaArtificial/conecta4/Maquina.java
+++ src/julian/inteligenciaArtificial/conecta4/Maquina.java
@@ -2,5 +2,14 @@ package julian.inteligenciaArtificial.conecta4;
 
 public class Maquina {
 	char color;
-
+	int numero;
+	
+	public Maquina(){
+		color = 'R';
+		numero = 1;
+	}
+	public int elegirColumna(){
+		// Algoritmo de búsqueda multiagente
+		return 0;
+	}
 }
