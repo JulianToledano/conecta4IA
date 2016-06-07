f4c28f0eec42541509a1924630cd69abed747295
Julian <jtoledanodiaz@gmail.com>
2016-06-07 19:09:07 +0200

    Numero cambia a turno, funcion en maquina para sacar su turno 1Âº

--- src/julian/inteligenciaArtificial/conecta4/Jugador.java
+++ src/julian/inteligenciaArtificial/conecta4/Jugador.java
@@ -1,14 +1,15 @@
+// Clase  que representa a la persona desde aquí el jugador elige la columna a la que insertar una ficha. 
 package julian.inteligenciaArtificial.conecta4;
 
 import java.util.Scanner;
 
 public class Jugador {
 	char color;
-	int numero;
+	int turno;
 	
 	public Jugador(){
 		color = 'A';
-		numero = 2;
+		turno = 2;
 	}
 	
 	public int elegirColumna(){
@@ -17,6 +18,8 @@ public class Jugador {
 		do{
 			columna = sc.nextInt();
 		}while(columna < 1 || columna > 8);
+		//sc.close();
 		return columna;
 	}
+
 }
--- src/julian/inteligenciaArtificial/conecta4/Maquina.java
+++ src/julian/inteligenciaArtificial/conecta4/Maquina.java
@@ -1,15 +1,19 @@
 package julian.inteligenciaArtificial.conecta4;
 
 public class Maquina {
-	char color;
-	int numero;
+	private char color;
+	private int turno;
 	
 	public Maquina(){
 		color = 'R';
-		numero = 1;
+		turno = 1;
 	}
 	public int elegirColumna(){
 		// Algoritmo de búsqueda multiagente
 		return 0;
+	}
+	
+	public int getTurno(){
+		return turno;
 	}
 }
