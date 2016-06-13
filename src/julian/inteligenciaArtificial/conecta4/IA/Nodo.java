0497c3ea9d4800f60be4f2fc2e56a8196b566c54
Julian <jtoledanodiaz@gmail.com>
2016-06-13 20:35:27 +0200

    clase nodo. correcta?

--- src/julian/inteligenciaArtificial/conecta4/IA/Nodo.java
+++ src/julian/inteligenciaArtificial/conecta4/IA/Nodo.java
@@ -5,7 +5,21 @@ import java.util.ArrayList;
 
 
 public class Nodo{
-	
+	int profundidad;
+	Tablero estadoActual;
+	Nodo padre;
+	ArrayList<Nodo>hijos;
 
+	public Nodo(int profundidad, Tablero actual, Nodo padre){
+		this.profundidad = profundidad;
+		estadoActual = actual;
+		this.padre = padre;
+		hijos = new ArrayList<Nodo>();
+	}
 	
+	public void insertarHijos(int columna, int jugador){
+		Nodo nodoHijo = new Nodo(this.profundidad+1, estadoActual, this);
+		nodoHijo.estadoActual.introducirFicha(columna,jugador);
+		hijos.add(nodoHijo);
+	}
 }
