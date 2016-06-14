e2c893f86d946db7158712850a209a5ec99b492b
Julian <jtoledanodiaz@gmail.com>
2016-06-14 17:54:46 +0200

    Borramos toda la IA.

--- src/julian/inteligenciaArtificial/conecta4/IA/AlphaBetaPruning.java
+++
@@ -1,7 +0,0 @@
-package julian.inteligenciaArtificial.conecta4.IA;
-
-import julian.inteligenciaArtificial.conecta4.Tablero;
-
-public class AlphaBetaPruning {
-
-}
--- src/julian/inteligenciaArtificial/conecta4/IA/Arbol.java
+++
@@ -1,9 +0,0 @@
-// Puede que también sea innecesaria. ¿Sería mejor crear el arbol a partir de la clase Nodo? Tal vez, 
-package julian.inteligenciaArtificial.conecta4.IA;
-
-import julian.inteligenciaArtificial.conecta4.Tablero;
-
-public class Arbol {
-
-	
-}
